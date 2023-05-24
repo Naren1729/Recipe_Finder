d82a26b83ea37053166f5b5b9e97a1fc1e66ad4e
naren sai <narensai29@gmail.com>
2023-05-24 16:17:52 +0530

    commit

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+
+
+
+export const logo = "https://thumbs.dreamstime.com/b/wooden-table-food-top-view-cafe-102532611.jpg"
+export const logo2 = "https://www.freepik.com/free-vector/recipe-book-concept-illustration_19245712.htm#query=recipe%20finder&position=41&from_view=search&track=ais"
+//export const logo =  './logo.jpg' ;
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
